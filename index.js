d940cc299bd29fde0902ad4580acb852695a86ff
Ajay David <antonio.g.david@gmail.com>
2017-04-04 09:49:34 -0400

    create React component with old syntax

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const BUTCHER_PRODUCTS = [
-  'Tenderloin',
-  'Short ribs',
-  'Beef shin',
-  'Ribeye'
-];
+const p1 = React.createElement('p', {}, 'Two grannies having the time of their life!');
+const p2 = React.createElement('p', {}, 'Passengers:');
+const list = React.createElement('ul', {}, [
+  React.createElement('li', {}, 'Agnes'),
+  React.createElement('li', {}, 'Muriel')
+]);
+const OlderCoaster = React.createClass({
+  render() {
+    return React.createElement('div', { className: 'oldercoaster'}, [p1, p2, list]);
+  }
+});
 
 ReactDOM.render(
-  React.createElement('div', {}, [
-    React.createElement(OlderCoaster),
-    React.createElement(InFrontOfYou),
-    React.createElement(ButcherShop)
-  ]),
+  React.createElement('div', {},
+    React.createElement(OlderCoaster)
+  ),
   document.getElementById('main')
 );
