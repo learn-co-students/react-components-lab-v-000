a307187d312bd9c22d93b0ce03792beecc0c946d
David Kennell <davidkennell0@gmail.com>
2017-06-14 15:28:03 -0400

    Done.

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const BUTCHER_PRODUCTS = [
 const OlderCoaster = React.createClass({
   render(){
     return React.createElement('div', {className: "oldercoaster"}, [
-    React.createElement('p', {}, 'Two grannies having the time of their life!'),
-    React.createElement('p', {}, 'Passengers:'),
+    React.createElement('p', {}, 'This is the old way to do components'),
+    React.createElement('p', {}, 'The one where we use const and createClass'),
+    React.createElement('p', {}, 'I\'m a p element, heres a fucking list:'),
     React.createElement('ul', {}, [
-      React.createElement('li', {}, 'Agnes'),
-      React.createElement('li', {}, 'Muriel')
+      React.createElement('li', {}, 'Thing 1'),
+      React.createElement('li', {}, 'Thing 2')
        ])
     ])
   }
@@ -22,9 +23,10 @@ const OlderCoaster = React.createClass({
 class InFrontOfYou extends React.Component {
   render(){
     return React.createElement('div', {}, [
-      React.createElement('p', {}, 'You shouldn\'t look too far.'),
-      React.createElement('p', {}, ['Sometimes, the solution is ', 
-      React.createElement('strong', {}, 'right in front of you.')
+      React.createElement('p', {}, 'This is the new way to do components.'),
+      React.createElement('p', {}, 'The one where we use classes (ie constructor functions) that extend React.Component.'),
+      React.createElement('p', {}, ['This is a p element', 
+      React.createElement('strong', {}, ' and this is a nested "strong" element, nested inside the p.')
       ])
         ])}
 }
@@ -32,7 +34,7 @@ class InFrontOfYou extends React.Component {
 class ButcherShop extends React.Component {
   render(){
     return React.createElement('div', {className: "butcher-shop"}, [
-      React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+      React.createElement('p', {}, 'We\'re using React.createElement here a bunch. Here\'s another list:'),
       React.createElement('ul', {}, [
         BUTCHER_PRODUCTS.map(function(product){
           return React.createElement('li', {}, product)
@@ -40,11 +42,20 @@ class ButcherShop extends React.Component {
   }
 }
 
+class Tweet extends React.Component {
+  render() {
+    return (
+            <p>We are writing this tweet in JSX.</p>  
+    );
+  }
+}
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
     React.createElement(InFrontOfYou),
-    React.createElement(ButcherShop)
-  ]),
+    React.createElement(ButcherShop),
+    React.createElement(Tweet)
+  ]), 
   document.getElementById('main')
 );
