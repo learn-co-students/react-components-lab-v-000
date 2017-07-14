adaa731641bddca8a40450418329e3d0b7486f7b
christinechoi <smi77y@gmail.com>
2017-07-13 21:56:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // Make sure to import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,15 +11,8 @@ const BUTCHER_PRODUCTS = [
 ];
 
 // Define these exported classes
+
 export class OlderCoaster extends React.Component {};
 export class InFrontOfYou extends React.Component {};
 export class ButcherShop extends React.Component {};
 
-ReactDOM.render(
-  React.createElement('div', {}, [
-    React.createElement(OlderCoaster),
-    React.createElement(InFrontOfYou),
-    React.createElement(ButcherShop)
-  ]),
-  document.getElementById('root')
-);
