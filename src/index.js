e636355574bb3a81a7d9fb636fe691d3582009d4
John Pfingst <jpfingst@outlook.com>
2017-08-17 11:07:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// Make sure to import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,9 +9,61 @@ const BUTCHER_PRODUCTS = [
 ];
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  // <div class="oldercoaster">
+  //   <p>Two grannies having the time of their life!</p>
+  //   <p>Passengers:</p>
+  //   <ul>
+  //     <li>Agnes</li>
+  //     <li>Muriel</li>
+  //   </ul>
+  // </div>
+  render() {
+    return React.createElement('div', { className: 'oldercoaster' },
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {},
+        [
+          React.createElement('li', {}, 'Agnes'),
+          React.createElement('li', {}, 'Muriel')
+        ]
+      ),
+    )
+  }
+};
+
+export class InFrontOfYou extends React.Component {
+  // <div>
+  //   <p>You shouldn't look too far.</p>
+  //   <p>Sometimes, the solution is right in front of you.</p>
+  // </div>
+  render() {
+    return React.createElement('div', {},
+      React.createElement('p', {}, "You shouldn't look too far."),
+      React.createElement('p', {}, 'Sometimes, the solution is right in front of you.')
+    )
+  }
+};
+
+var ButcherShopListItems = BUTCHER_PRODUCTS.map(function(item) { return React.createElement('li', {}, item) });
+
+export class ButcherShop extends React.Component {
+//   <div class="butcher-shop">
+//   <p>Hello! We have the following products for sale today:</p>
+//   <ul>
+//     <li>Tenderloin</li>
+//     <li>Short ribs</li>
+//     <li>Beef shin</li>
+//     <li>Ribeye</li>
+//   </ul>
+// </div>
+  render() {
+    return React.createElement('div', { className: 'butcher-shop' },
+      React.createElement('p',  {}, 'Hello! We have the following products for sale today:'),
+      React.createElement('ul', {}, ButcherShopListItems),
+    )
+  }
+};
 
 ReactDOM.render(
   React.createElement('div', {}, [
