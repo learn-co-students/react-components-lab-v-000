df4a001a5c256034586c5e9007c65a58284647a2
Timothy Cevallos <timcev@gmail.com>
2017-10-25 01:50:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ export class ButcherShop extends React.Component {
       React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
       React.createElement('ul', {},
         BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
+
       )
     ]);
   }
