d571b0fc8c9589f3b625eb2e6e449f36e8f5424d
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-27 19:26:20 -0800

    bullet points rendered via mapping

--- src/index.js
+++ src/index.js
@@ -37,13 +37,9 @@ export class ButcherShop extends React.Component {
     return React.createElement('div', {className:'butcher-shop'},
       React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
       React.createElement('ul', {}, [
-        React.createElement('li', {}, 'Tenderloin'),
-        React.createElement('li', {}, 'Short ribs'),
-        React.createElement('li', {}, 'Beef shin'),
-        React.createElement('li', {}, 'Ribeye')
-        ]
+        BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
       )
-    )
+    ]);
   }
 };
 
