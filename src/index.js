5ba149d127824b3ad5114deb14aebad5be9acc11
Hannah Carnes <hecarnes@gmail.com>
2018-03-14 15:03:39 -0400

    renders oldercoaster class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // Make sure to import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,9 +10,30 @@ const BUTCHER_PRODUCTS = [
 ];
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  render() {
+    return React.createElement('div', { className: "oldercoaster" },
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, 
+      [
+        React.createElement('li', {}, 'Agnes'),
+        React.createElement('li', {}, 'Muriel'),
+      ])
+    )
+  }
+};
+
+export class InFrontOfYou extends React.Component {
+  render() {
+    return <h1> Hi </h1>
+  }
+};
+export class ButcherShop extends React.Component {
+  render() {
+    return <h1> Hi </h1>
+  }
+};
 
 ReactDOM.render(
   React.createElement('div', {}, [
