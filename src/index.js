fce0f7eced4736dc92e69a3da1aabbdcadc26996
Hannah Carnes <hecarnes@gmail.com>
2018-03-14 15:31:28 -0400

    refactor meat listing

--- src/index.js
+++ src/index.js
@@ -35,18 +35,14 @@ export class InFrontOfYou extends React.Component {
 
 export class ButcherShop extends React.Component {
   render() {
-    return React.createElement('div', { className: "butcher-shop" },
+    return React.createElement('div', { className: "butcher-shop" }, 
       React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
-      React.createElement('ul', {}, 
-      [
-        React.createElement('li', {}, 'Tenderloin'),
-        React.createElement('li', {}, 'Short ribs'),
-        React.createElement('li', {}, 'Beef shin'),
-        React.createElement('li', {}, 'Ribeye'),
-      ])
-    )
+      React.createElement('ul', {}, BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
+      )
+    );
   }
-};
+}
+
 
 ReactDOM.render(
   React.createElement('div', {}, [
