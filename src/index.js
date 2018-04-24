c142666a3ea2f7df9c8e78a3cec743012edd8d5e
Sophia Varella <sophiavarella@gmail.com>
2018-04-24 15:34:40 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
@@ -5,19 +8,56 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
-// Define these exported classes
 export class OlderCoaster extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
+
+export class Product extends React.Component {
+  render() {
+    return <li>{this.props.product}</li>
+  }
+}
+
+//stateless functional components
+// const Product = ({ product }) => {
+//   return <li>{product}</li>
+// }
 
 export class ButcherShop extends React.Component {
-  // your code here
+  render() {
+    const products = BUTCHER_PRODUCTS.map((product) => <Product product={product}/>)
+    return(
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
 }
-
 
 ReactDOM.render(
   <div>
