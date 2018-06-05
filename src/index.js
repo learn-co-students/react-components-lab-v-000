80cdc06d78b4f3abb20caabd724bf42847997349
jacobsilver2 <jacobsilver2@mac.com>
2018-06-05 17:54:44 -0400

    second

--- src/index.js
+++ src/index.js
@@ -41,15 +41,12 @@ export class InFrontOfYou extends React.Component {
 }
 
 export class ButcherShop extends React.Component {
+  const products = 
   render() {
     return (
       <div className="butcher-shop">
         <p>Hello! We have the following products for sale today:</p>
         <ul>
-          <li>Tenderloin</li>
-          <li>Short ribs</li>
-          <li>Beef shin</li>
-          <li>Ribeye</li>
         </ul>
       </div>
     )
