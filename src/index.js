b875f46fd7851fd3d697c6b0da223d158222f1e1
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-11 16:13:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -10,30 +10,64 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  
+  render() {
+    return (
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
   // this function should return the JSX we want the DOM to render
   // render() {
   //   return (
   //     <div>Hello!</div>
   //   )
   // }
-  // 
+  //
 }
 
 export class InFrontOfYou extends React.Component {
   // your code here
+  render() {
+    return (
+      <div>
+        <p>You shouldn&#x27;t look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends React.Component {
   // your code here
+  render() {
+    const products = BUTCHER_PRODUCTS.map((product) => (<li>{product}</li>))
+    return (
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
 }
+
+
 
 
 export class App extends React.Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
