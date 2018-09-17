75bb89a3f2a8a490f7f2bed64a08d19b42ee2612
nyuyz518 <lilyzhangyun@gmail.com>
2018-09-16 20:59:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -8,35 +8,63 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
-// Define these exported classes
 export class OlderCoaster extends Component {
+    // this function should return the JSX we want the DOM to render
+    render() {
+      return (
+        <div class="oldercoaster">
+          <p>Two grannies having the time of their life!</p>
+          <p>Passengers:</p>
+          <ul>
+            <li>Agnes</li>
+            <li>Muriel</li>
+          </ul>
+        </div>
+      )
+    }
+  }
 
-  // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
-}
 
 export class InFrontOfYou extends Component {
-  // your code here
-}
-
-export class ButcherShop extends Component {
-  // your code here
-}
-
-
-export class App extends Component {
-  render() {
-    return (
-      <div id="app">
-
-      </div>
-    )
+    // your code here
+    render(){
+      return(
+        <div>
+          <p>You shouldn't look too far.</p>
+          <p>Sometimes, the solution is right in front of you.</p>
+        </div>
+      )
+    }
   }
-}
+
+
+  export class ButcherShop extends Component {
+    render(){
+      return(
+        <div class="butcher-shop">
+          <p>Hello! We have the following products for sale today:</p>
+          <ul>
+            <li>Tenderloin</li>
+            <li>Short ribs</li>
+            <li>Beef shin</li>
+            <li>Ribeye</li>
+          </ul>
+        </div>
+      )
+    }
+  }
+
+  export class App extends Component {
+    render() {
+      return (
+        <div id="app">
+          <OlderCoaster />
+          <InFrontOfYou />
+          <ButcherShop />
+        </div>
+      )
+    }
+  }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
