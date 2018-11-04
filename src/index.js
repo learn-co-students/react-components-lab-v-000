ea28c47cc5700cf015e92db6c849be6795378a63
github username <schun08@gmail.com>
2018-11-04 16:33:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,57 @@ const BUTCHER_PRODUCTS = [
   'Beef shin',
   'Ribeye'
 ]
-
+// First attempt 4/27/18
 // Define these exported classes
+// export class OlderCoaster extends React.Component {
+//   render() {
+//     return (
+//       <div class="oldercoaster">
+//         <p>Two grannies having the time of their life!</p>
+//         <p>Passengers:</p>
+//         <ul>
+//           <li>Agnes</li>
+//           <li>Muriel</li>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+//
+// export class InFrontOfYou extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//       <p>You shouldn't look too far.</p>
+//       <p>Sometimes, the solution is right in front of you.</p>
+//       </div>
+//     )
+//   }
+// }
+//
+// export class ButcherShop extends React.Component {
+//   render() {
+//     const products = BUTCHER_PRODUCTS.map((p, idx) => (<li key={idx}>{p}</li>))
+//     return (
+//       <div class="butcher-shop">
+//         <p>Hello! We have the following products for sale today:</p>
+//         <ul>
+//           { products }
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+//
+//
+// ReactDOM.render(
+//   <div>
+//     render your components here! Refer back to the previous README if you are lost
+//   </div>,
+//   document.getElementById('root')
+// )
+
+// Second attempt 11/4/18
 export class OlderCoaster extends React.Component {
   render() {
     return (
@@ -28,8 +77,8 @@ export class InFrontOfYou extends React.Component {
   render() {
     return (
       <div>
-      <p>You shouldn't look too far.</p>
-      <p>Sometimes, the solution is right in front of you.</p>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
       </div>
     )
   }
@@ -49,10 +98,16 @@ export class ButcherShop extends React.Component {
   }
 }
 
+export class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(
-  <div>
-    render your components here! Refer back to the previous README if you are lost
-  </div>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
